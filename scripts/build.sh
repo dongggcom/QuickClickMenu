8f28429466e094eca6e0a079f61739efe6ac96b6
Simon Dong <1961396030@qq.com>
2024-04-25 11:45:55 +0800

    build: modify build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,7 @@
-bun build src/index.ts --outdir dist 
-cp src/style.less dist/style.less
+OUTPUT_DIR=dist
+
+rm -rf $OUTPUT_DIR
+
+bun build src/index.ts --outdir $OUTPUT_DIR --entry-naming "[name].esm.[ext]"   --asset-naming "[name].[ext]"
+
+lessc --js --silent src/style.less $OUTPUT_DIR/style.css
